e70c8d32d6a9f66ec15ca9e36af1a3247be5590f
n1260263 <bgdm1260263@gn.iwasaki.ac.jp>
2018-09-27 10:35:01 +0900

    Comment in

--- ango.py
+++ ango.py
@@ -1,3 +1,4 @@
+# Words Array
 figures = [
 'A','B','C','D','E','F','G',
 'H','I','J','K','L','M','N',
@@ -5,18 +6,31 @@ figures = [
 'V','W','X','Y','Z'
 ]
 
+# Loop endless untill you input "exit"
 while True:
+	
+	# INPUT Message
 	mes_in = input()
 	
-	if mes_in != "exit":	
+	# Continue or NOT
+	if mes_in != "exit":
+	
+		# Initialize OUTPUT Message
 		mes_out = ""
-
+		
+		# Checking each figures
 		for j in range(len(mes_in)):
+			
+			# Find out a INPUT figure from 'figures' array
 			for i in range(len(figures)):
+			
+				# Found a figure, add to OUTPUT Message and finish searching
 				if(mes_in[j] == figures[i]):
 					mes_out += figures[(i+10)%26]
 					break
 		
+		# Display Changing
 		print (mes_in + " -> " + mes_out)
+	
 	else:
 		exit()
